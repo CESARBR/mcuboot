be39f9260dbde6263a457c556851a33645f30193
Joao Cordeiro <jvcc@cesar.org.br>
2018-11-29 12:53:50 -0300

    boot: zephyr: Use printk based  mcuboot_logging.h
    Using mcuboot_logging based on mynewt's. It is a exact copy, except
    by the fact that it doesn't support default log level configuration
    and uses printk instead of printf (which is not supported by zephyr).
    
    Signed-off-by: Joao Cordeiro <jvcc@cesar.org.br>

--- boot/zephyr/include/mcuboot_config/mcuboot_logging.h
+++ boot/zephyr/include/mcuboot_config/mcuboot_logging.h
@@ -1,13 +1,32 @@
 /*
- * Copyright (c) 2018 Runtime Inc
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * SPDX-License-Identifier: Apache-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
+#include <zephyr.h>
 
 #ifndef __MCUBOOT_LOGGING_H__
 #define __MCUBOOT_LOGGING_H__
 
-#ifndef __BOOTSIM__
+#define BOOTUTIL_LOG_LEVEL_OFF     1
+#define BOOTUTIL_LOG_LEVEL_ERROR   2
+#define BOOTUTIL_LOG_LEVEL_WARNING 3
+#define BOOTUTIL_LOG_LEVEL_INFO    4
+#define BOOTUTIL_LOG_LEVEL_DEBUG   5
 
 /*
  * When building for targets running Zephyr, delegate to its native
@@ -24,6 +43,13 @@
 
 #include <logging/log.h>
 
-#endif /* !__BOOTSIM__ */
+#if MCUBOOT_LOG_LEVEL >= MCUBOOT_LOG_LEVEL_DEBUG
+#define MCUBOOT_LOG_DBG(_fmt, ...)                                      \
+    do {                                                                \
+        printk("[DBG] " _fmt "\n", ##__VA_ARGS__);                      \
+    } while (0)
+#else
+#define MCUBOOT_LOG_DBG(...) IGNORE(__VA_ARGS__)
+#endif
 
-#endif /* __MCUBOOT_LOGGING_H__ */
+#endif
