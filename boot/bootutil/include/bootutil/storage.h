757e99a53bff56015e1c55f4829d6f959dc7c52d
Marcio Montenegro <msam@cesar.org.br>
2019-01-07 11:49:21 -0300

    boot: zephyr: Update mcuboot to use KNoT storage
    Mcuboot and KNoT app shares the same storage code
    
    Signed-off-by: Marcio Montenegro <msam@cesar.org.br>

--- boot/bootutil/include/bootutil/storage.h
+++ boot/bootutil/include/bootutil/storage.h
@@ -1,34 +1,39 @@
+/* storage.h - KNoT Thing Storage handler */
+
 /*
  * Copyright (c) 2018, CESAR. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef STORAGE_H
-#define STORAGE_H
+#include  "bootutil/knot_protocol.h"
+
+#define STORAGE_NET_NAME_LEN 16
+#define STORAGE_XPANID_LEN 23
+#define STORAGE_MASTERKEY_LEN 47
+#define STORAGE_PEER_IPV6_LEN 39
+
+struct storage_app_settings {
+	uint64_t device_id;
+	char uuid[KNOT_PROTOCOL_UUID_LEN];
+	char token[KNOT_PROTOCOL_TOKEN_LEN];
+};
+
+struct storage_pan_settings {
+	bool need_config;			// PAN info needs settings
+	uint16_t pan_id;			// OpenThread PAN Id
+	uint16_t channel;			// OpenThread channel
+	char net_name[STORAGE_NET_NAME_LEN];	// OpenThread net name
+	char xpanid[STORAGE_XPANID_LEN];	// OpenThread expanded PAN Id
+	char masterkey[STORAGE_MASTERKEY_LEN];	// OpenThread master key
+	char peer_ipv6[STORAGE_PEER_IPV6_LEN];	// OpenThread peer's ipv6
+};
 
 enum storage_key {
-    STORAGE_KEY_ID		= 0xFFFF,
-    STORAGE_KEY_UUID	= 0xFFFE,
-    STORAGE_KEY_TOKEN	= 0xFFFD,
-    STORAGE_KEY_NET_SETTINGS = 0xFFFA
+	STORAGE_APP_SETTINGS_KEY     = 0xFFFF,	/* App related settings */
+	STORAGE_PAN_SETTINGS_KEY     = 0xFFFE,	/* PAN related settings */
 };
 
-struct net_settings {
-        /** Indicate that network settings is configured */
-        u8_t    setup;
-        /** Put openthread params here */
-};
 /**
  * @brief Prepare environment to use flash memory.
  *
@@ -42,7 +47,7 @@ int8_t storage_init(void);
 /**
  * @brief Delete all stored values.
  *
- * @details Delete info for all known keys for this file system.
+ * @details Delete info for all known IDs for this file system.
  *
  * @retval 0 Success
  * @retval -ERRNO errno code if error
@@ -52,36 +57,29 @@ int8_t storage_reset(void);
 /**
  * @brief Gets value from NVM.
  *
- * @details Get value of a key from flash memory.
+ * @details Gets UUID, TOKEN and MAC from flash memory.
  *
  * @param Int key for the value to be retrieved.
- * @param net_config struct to store read value.
+ * @param Buffer string to be stored.
+ * @param Size buffer len.
  *
  * @retval 0 for SUCCESS.
- * @retval -EINVAL for Error.
+ * @retval -ERRNO errno code for Error.
  */
 
-int8_t storage_get(enum storage_key key, struct net_settings *net_config);
+int8_t storage_get(enum storage_key key, u8_t *value);
 
 /**
- * @brief Save key value on NVM.
+ * @brief Save value on NVM.
  *
- * @details Save on flash memory.
+ * @details Save UUID, TOKEN and MAC on flash memory.
  *
- * @param Int key for the value to be saved.
- * @param net_config struct to save.
+ * @param Int key for the value to be retrieved.
+ * @param Buffer string to be stored.
+ * @param Size buffer len.
  *
  * @retval 0 for SUCCESS.
  * @retval -1 for INVALID KEY.
  */
 
-int8_t storage_set(enum storage_key key, const struct net_settings *net_config);
-
-// Allocating last 2 blocks of storage area
-#define MCUBOOT_STORAGE_TOTAL	(FLASH_AREA_STORAGE_OFFSET + \
-                                FLASH_AREA_STORAGE_SIZE)
-
-#define MCUBOOT_STORAGE_OFFSET  (MCUBOOT_STORAGE_TOTAL - \
-                                ( 2 * FLASH_ERASE_BLOCK_SIZE))
-
-#endif
+int8_t storage_set(enum storage_key key, const u8_t *value);
--- boot/bootutil/src/storage.c
+++ boot/bootutil/src/storage.c
@@ -1,60 +1,31 @@
+/* storage_mock.c - KNoT Thing Storage MOCK handler */
+
 /*
  * Copyright (c) 2018, CESAR. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
+
+/*
+ * This file works as a mock-up for the storage on NVM. It is intended to be
+ * used for testing only.
+ * To use with the nRF52840 board, use the storage.nrf52840 file.
+ */
+
 #include <zephyr.h>
-#include <nvs/nvs.h>
-#include <bootutil/storage.h>
+#include <net/net_core.h>
+#include <string.h>
 
-/* Mininum required flash parameters */
-#define NVS_SECTOR_SIZE         FLASH_ERASE_BLOCK_SIZE
-#define NVS_SECTOR_COUNT        2
+#include  "bootutil/storage.h"
+#include  "bootutil/knot_protocol.h"
 
-static struct nvs_fs mcuboot_fs = {
-    .sector_size = NVS_SECTOR_SIZE,
-    .sector_count = NVS_SECTOR_COUNT,
-    .offset = MCUBOOT_STORAGE_OFFSET,
-};
+#if CONFIG_LOG
+    #include <logging/log.h>
+    LOG_MODULE_DECLARE(knot, LOG_LEVEL_DBG);
+#endif
 
-int8_t storage_init(void)
-{
-    return nvs_init(&mcuboot_fs, DT_FLASH_DEV_NAME);
-}
-
-int8_t storage_reset(void)
-{
-    return nvs_delete(&mcuboot_fs, STORAGE_KEY_NET_SETTINGS);
-}
-
-int8_t storage_get(u16_t key, struct net_settings *net_config)
-{
-    ssize_t len =  sizeof(*net_config);
-    ssize_t read_len;
-
-    if (key != STORAGE_KEY_NET_SETTINGS)
-            return -EINVAL;
-
-    read_len = nvs_read(&mcuboot_fs, key, net_config, len);
-    if (read_len != len)
-        return -EINVAL;
-
-    /* Successful read */
-    return 0;
-}
-
-int8_t storage_set(u16_t key, const struct net_settings *net_config)
-{
-	return nvs_write(&mcuboot_fs, key, (void *) net_config,
-			 sizeof(*net_config));
-}
+#if CONFIG_BOARD_NRF52840_PCA10056
+	#include "storage.nrf52840"
+#elif CONFIG_BOARD_QEMU_X86
+	#include "storage.qemu"
+#endif
